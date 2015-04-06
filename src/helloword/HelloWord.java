d3685a846651d72ee00291cc7b3954d5a16ffcb8
Asus-siwi <Asus-siwi@Siwi>
2015-04-07 06:17:55 +0700

    new commit

---
+++ src/helloword/HelloWord.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloword;
+
+/**
+ *
+ * @author Asus-siwi
+ */
+public class HelloWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello,siwi rahatri utami, 6306130080 saya sangat suka praktikum rpl modul 8 ini");
+    }
+}
